dcf2affa775b43abfd94f6f878742beaa74c4fc8
vladsaveliev <vladislav.sav@gmail.com>
2018-03-05 23:12:06 +1100

    Refactor genomes structure

--- python_utils/hpc.py
+++ python_utils/hpc.py
@@ -14,101 +14,121 @@ def find_loc():
     Loc = collections.namedtuple('Loc',
          'name '
          'host_pattern '
-         'hsapiens '
          'extras '
-         'panel_of_normals_dir '
-         'truth_sets ')
+         'genomes '
+    )
 
     hostname = socket.gethostname()
     loc_by_name = {
         'spartan': Loc(
             name='spartan',
             host_pattern=r'spartan.*\.hpc\.unimelb\.edu\.au',
-            hsapiens='/data/projects/punim0010/local/share/bcbio/genomes/Hsapiens',
             extras='/data/cephfs/punim0010/extras',
-            panel_of_normals_dir='/data/cephfs/punim0010/extras/panel_of_normals',
-            truth_sets={
-                'mb': {
-                    'GRCh37': {
-                        'vcf': '/data/cephfs/punim0010/data/External/Reference/ICGC_MB/MB-benchmark.vcf.gz',
-                    }
-                },
-                'dream': {
-                    'GRCh37': {
-                        'vcf': 'GRCh37/validation/dream-syn3/truth_small_variants.vcf.gz',
-                        'bed': 'GRCh37/validation/dream-syn3/truth_regions.bed',
-                    }
-                },
-                'giab': {
-                    'GRCh37': {
-                        'vcf': 'GRCh37/validation/giab-NA12878/truth_small_variants.vcf.gz',
-                        'bed': 'GRCh37/validation/giab-NA12878/truth_regions.bed',
-                    }
-                },
-                'colo': {
-                    'GRCh37': {
-                        'vcf': '/data/cephfs/punim0010/data/External/Reference/COLO829_Craig/truth_set/EGAZ00001226241_ListforNatureReports.IndelsandSNVs.final.Suppl1.snpEff.validated.SORTED.vcf'
-                    }
-                }
+            genomes={
+                'GRCh37': dict(
+                    fa=   '/data/projects/punim0010/local/share/bcbio/genomes/Hsapiens/GRCh37/seq/GRCh37.fa',
+                    az300='/data/projects/punim0010/local/share/bcbio/genomes/Hsapiens/GRCh37/coverage/prioritize/cancer/az300.bed.gz',
+                    panel_of_normals_dir='/data/cephfs/punim0010/extras/panel_of_normals',
+                    truth_sets={
+                        'mb': {
+                            'vcf': '/data/cephfs/punim0010/data/External/Reference/ICGC_MB/MB-benchmark.vcf.gz',
+                        },
+                        'dream': {
+                            'vcf': 'GRCh37/validation/dream-syn3/truth_small_variants.vcf.gz',
+                            'bed': 'GRCh37/validation/dream-syn3/truth_regions.bed',
+                        },
+                        'giab': {
+                            'vcf': 'GRCh37/validation/giab-NA12878/truth_small_variants.vcf.gz',
+                            'bed': 'GRCh37/validation/giab-NA12878/truth_regions.bed',
+                        },
+                        'colo': {
+                            'vcf': '/data/cephfs/punim0010/data/External/Reference/COLO829_Craig/truth_set/EGAZ00001226241_ListforNatureReports.IndelsandSNVs.final.Suppl1.snpEff.validated.SORTED.vcf'
+                        }
+                    },
+                ),
             },
+
         ),
         'raijin': Loc(
             name='raijin',
             host_pattern=r'^raijin|(r\d\d\d\d$)',
-            hsapiens='/g/data/gx8/local/development/bcbio/genomes/Hsapiens',
-            extras='/g/data3/gx8/extras',
-            panel_of_normals_dir='/g/data3/gx8/extras/panel_of_normals',
-            truth_sets={
-                'giab': {
-                    'GRCh37': {
-                        'vcf': 'GRCh37/validation/giab-NA12878/truth_small_variants.vcf.gz',
-                        'bed': 'GRCh37/validation/giab-NA12878/truth_regions.bed',
+            genomes={
+                'GRCh37': dict(
+                    fa='/g/data/gx8/local/development/bcbio/genomes/Hsapiens/GRCh37/seq/GRCh37.fa',
+                    az300='{g}/coverage/prioritize/cancer/az300.bed.gz',
+                    panel_of_normals_dir='/g/data3/gx8/extras/panel_of_normals',
+                    truth_sets={
+                        'giab': {
+                            'vcf': '{g}/validation/giab-NA12878/truth_small_variants.vcf.gz',
+                            'bed': '{g}/validation/giab-NA12878/truth_regions.bed',
+                        }
                     }
-                }
-            }
+                ),
+            },
+            extras='/g/data3/gx8/extras',
         ),
         'vlad': Loc(
             name='vlad',
             host_pattern=r'^5180L-135800-M.local$',
-            hsapiens='/Users/vsaveliev/googledrive/bio/reference_data/genomes/Hsapiens',
             extras='/Users/vsaveliev/googledrive/bio/extras',
-            panel_of_normals_dir='/Users/vsaveliev/googledrive/bio/extras/panel_of_normals/GRCh37',
-            truth_sets={
-                'mb': {
-                    'GRCh37': {
-                        'vcf': '/Users/vsaveliev/googledrive/bio/extras/ICGC_MB/truth_small_variants.vcf.gz',
+            genomes={
+                'GRCh37': dict(
+                    fa=   '/Users/vsaveliev/googledrive/bio/reference_data/genomes/Hsapiens/GRCh37/seq/GRCh37.fa',
+                    az300='/Users/vsaveliev/googledrive/bio/reference_data/genomes/Hsapiens/GRCh37/coverage/prioritize/cancer/az300.bed.gz',
+                    panel_of_normals_dir='/Users/vsaveliev/googledrive/bio/extras/panel_of_normals/GRCh37',
+                    truth_sets={
+                        'mb': {
+                            'vcf': '/Users/vsaveliev/googledrive/bio/extras/ICGC_MB/truth_small_variants.vcf.gz',
+                        },
+                        'giab': {
+                            'vcf': '/Users/vsaveliev/googledrive/bio/reference_data/genomes/Hsapiens/GRCh37/validation/giab-NA12878/truth_small_variants.vcf.gz',
+                            'bed': '/Users/vsaveliev/googledrive/bio/reference_data/genomes/Hsapiens/GRCh37/validation/giab-NA12878/truth_regions.bed',
+                        },
                     }
-                },
-                'giab': {
-                    'GRCh37': {
-                        'vcf': '/Users/vsaveliev/googledrive/bio/reference_data/genomes/Hsapiens/GRCh37/validation/giab-NA12878/truth_small_variants.vcf.gz',
-                        'bed': '/Users/vsaveliev/googledrive/bio/reference_data/genomes/Hsapiens/GRCh37/validation/giab-NA12878/truth_regions.bed',
-                    },
-                }
-            }
+                ),
+                'test-GRCh37': dict(
+                    fa='/Users/vsaveliev/git/umccr/vcf_stuff/tests/data/test-GRCh37.fa',
+                    panel_of_normals_dir='/Users/vsaveliev/googledrive/bio/extras/panel_of_normals/GRCh37',
+                    truth_sets={
+                        'test-mb': {
+                            'vcf': '/Users/vsaveliev/git/umccr/vcf_stuff/tests/data/test-benchmark.vcf.gz',
+                        }
+                    }
+                )
+            },
         ),
         'travis': Loc(
             name='travis',
             host_pattern=r'^travis-',
-            hsapiens='../../data/genomes/Hsapiens',
-            # For tests, using the following in Hsapiens:
-            # For germline subsampling: 'GRCh37/coverage/prioritize/cancer/az300.bed.gz'
-            # For goleft depth:         'GRCh37/seq/GRCh37.fa
-            # For bedtools slop :       'GRCh37/seq/GRCh37.fa.fai'
             extras='',
-            panel_of_normals_dir='../../data/panel_of_normals',
-            truth_sets={
-                'giab': {
-                    'GRCh37': {
-                        'bed': 'GRCh37/validation/giab-NA12878/truth_regions.bed',
+            genomes={
+                'GRCh37': dict(
+                    # .fa for goleft depth and VCF normalisation:
+                    fa='../../data/genomes/Hsapiens/GRCh37/seq/GRCh37.fa',
+                    # for germline subsampling:
+                    az300='../../data/genomes/Hsapiens/GRCh37/coverage/prioritize/cancer/az300.bed.gz',
+                    panel_of_normals_dir='../../data/panel_of_normals',
+                    truth_sets={
+                        'giab': {
+                            'bed': '../../data/genomes/Hsapiens/GRCh37/validation/giab-NA12878/truth_regions.bed',
+                        },
+                        'test-mb': {
+                            'test-GRCh37': {
+                                'vcf': '../../data/test-benchmark.vcf.gz',
+                            }
+                        }
+                    },
+                ),
+                'test-GRCh37': dict(
+                    fa='tests/data/test-GRCh37.fa',
+                    panel_of_normals_dir='tests/data/panel_of_normals/GRCh37',
+                    truth_sets={
+                        'test-mb': {
+                            'vcf': 'tests/data/test-benchmark.vcf.gz',
+                        }
                     }
-                },
-                'mb': {
-                    'GRCh37': {
-                        'vcf': '/data/cephfs/punim0010/data/External/Reference/ICGC_MB/MB-benchmark.vcf.gz',
-                    }
-                }
-            }
+                )
+            },
         ),
     }
     if 'TRAVIS' in os.environ.keys():
@@ -127,3 +147,12 @@ def get_loc():
         return loc
     else:
         raise Exception('Could not find loc for hostname ' + socket.gethostname())
+
+
+def get_ref(path):
+    """ If path does not exist, search it in bcbio/genome/Hsapiens location
+    """
+    path = path.format()
+    if not os.path.isfile(path):
+        return os.path.join(get_loc().hsapiens, path)
+    return path
